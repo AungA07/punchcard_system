bc8ffeac56dee7bc66c6bf34d83bd667ab447941
Aung Aung <aaung8@ivytech.edu>
2024-09-30 21:42:37 -0400

    Version of punchcard system that will work with SQLite database. It is in the works.

--- punchcard_system/punchcard.py
+++ punchcard_system/punchcard.py
@@ -1,29 +1,43 @@
 from datetime import datetime
+import sqlite3
+# testing purposes
+from time import sleep
 
+path = r"C:\programing\anthis_code\2024-2025\punchcard_system\test_database\cosmetology_records.db"
 class Clocking:
-    """Using database as a test. the database argument would be replaced with however we access the actual database"""
-    working = False
+    connection = sqlite3.Connection(path)
 
-    def __init__(self, id:int, database:dict) -> None:
-        # id could also be the name or whatever would be used to identify the person
-        self.id = database[id]
+    def __init__(self, id:int) -> None:
+        self.id = id
+        self.working = self.connection.execute(f"SELECT working FROM students WHERE id = {self.id}")
 
     def punch_in(self):
-        if self.working:
-            # no punching in again if you're working
+        if self.working == 1:
+            # no punching in again if you're working (1 means true)
             return
-        self.working = True
-        self.id["start_time"] = datetime.now()
-    
-    def punch_out(self):
-        if not self.working:
-            # no punching out if you're not working
-            return
-        self.working = False
-        self.id["end_time"] = datetime.now()
+        # change working status in db
+        self.connection.execute(f"UPDATE students SET working = 1 WHERE id = {self.id}")
+        start_time = str(datetime.now())
+        # add/update start time in database
+        self.connection.execute(f"UPDATE students SET start_time = '{start_time}' WHERE id = {self.id}")
 
-        # this value would be shown to the user
-        time_worked = self.id["end_time"] - self.id["start_time"]
-        # add to running total
-        self.id["total_time"] += time_worked.total_seconds()
-        print(f"seconds worked: {time_worked.total_seconds()}")
+    def punch_out(self):
+        if self.working == 0:
+            return
+        self.connection.execute(f"UPDATE students SET working = 0 WHERE id = {self.id}")
+        end_time = str(datetime.now())
+        self.connection.execute(f"UPDATE students SET end_time = '{end_time}' WHERE id = {self.id}")
+        time_worked = self.connection.execute() # get end_time value -> convert back to datetime obj and then subtract start_time (also change to datetime)
+        total_time = self.connection.execute() # get total time value
+        total_time += time_worked
+        self.connection.execute() # update total time
+
+time = str(datetime.now())
+print(time)
+# converts string back into datetime object
+time = datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+sleep(3)
+time_now = datetime.now()
+print(time_now)
+time_difference = time_now - time
+print(time_difference.total_seconds())
